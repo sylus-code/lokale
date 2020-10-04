f337718383d3c828b067bb2b57ff4827fd98f5b3
cenee <ceneemail@gmail.com>
2020-10-04 21:24:38 +0200

    small visual update to call-to-action section

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -2,16 +2,16 @@ import React, {Component} from 'react';
 
 class CallToAction extends Component {
     render() {
-        const offer = ["Sprzedaż mieszkań", "Sprzedaż mieszkań na rynku wtórnym", "Wynajem mieszkań i lokali użytkowych", "rynek pierowtny i wtórny", "Sprzedaż mieszkań na rynku wtórnym", "Kredyty hipoteczne na zakup domu i mieszkania"];
+        const offer = ["Sprzedaż mieszkań", "Sprzedaż mieszkań na rynku wtórnym", "Wynajem mieszkań i lokali użytkowych", "rynek pierwotny i wtórny", "Sprzedaż mieszkań na rynku wtórnym", "Kredyty hipoteczne na zakup domu i mieszkania"];
 
         return (
             <section className="call-to-action">
                 <div className="container">
                     <div className="row ">
-                        <div>
-                            <p className="text-left text-uppercase px-1">{offer[3]}</p>
+                        <div className='col-12'>
+                            <p className="text-left text-uppercase px-auto">{offer[3]}</p>
                             <h1 className="text-secondary text-uppercase pl-2 pb-3 ">{offer[0]}</h1>
-                            <p className="text-right text-uppercase px-1">{offer[2]}</p>
+                            <p className="text-right text-uppercase px-auto">{offer[2]}</p>
                         </div>
                     </div>
                 </div>
