8a227d3c057682a081a456afcef1ac712daca655
cenee <ceneemail@gmail.com>
2020-10-14 14:39:33 +0200

    add clear form after sending email successfully

--- src/components/OfferFormik.js
+++ src/components/OfferFormik.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import {Formik, Field, Form} from 'formik';
 
 class OfferFormik extends Component {
-    sendEmail(values) {
+
+    sendEmail(values, {resetForm}) {
         fetch('/mail', {
             method: "POST",
             body: JSON.stringify(values),
@@ -15,10 +16,12 @@ class OfferFormik extends Component {
         ).then((response) => {
             if (response.status === 'success') {
                 alert("Wiadomość została wysłana.");
-                this.resetForm();
+                resetForm({values: ''});
             } else if (response.status === 'error') {
                 alert("Nie udało się wysłać wiadomości. Sprawdź poprawność danych.")
             }
+        }).catch(error => {
+            console.error('Nie da się zrobić sukcesu', error);
         })
     }
 
@@ -27,7 +30,7 @@ class OfferFormik extends Component {
         if (!value) {
             error = 'Wpisz adres email';
         } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
-            error = 'Invalid email address';
+            error = 'Nieprawidłowy adres e-mail';
         }
         return error;
     }
@@ -35,7 +38,7 @@ class OfferFormik extends Component {
     validateUsername(value) {
         let error;
         if (value === 'admin' || value === 'Admin') {
-            error = 'Nice try!';
+            error = 'Nieźle! ale nie tym razem ;)';
         }
         if (!value) {
             error = 'Wpisz swoje imię';
@@ -46,7 +49,14 @@ class OfferFormik extends Component {
     }
 
     render() {
-
+        const initialValues = {
+            name: '',
+            email: '',
+            tel: '',
+            field: '',
+            localisation: '',
+            budget: ''
+        };
         return (
             <section className="text-dark text-center mt-5" id='contact'>
                 <div className="overlay"></div>
@@ -58,24 +68,19 @@ class OfferFormik extends Component {
                         <div className="col-md-12 col-lg-12 col-xl-8 mx-auto mb-5 mt-3">
 
                             <Formik
-                                initialValues={{
-                                    name: '',
-                                    email: '',
-                                    tel: '',
-                                    field: '',
-                                    localisation: '',
-                                    budget: ''
-                                }}
-                                onSubmit={async (values) => {
+                                initialValues={initialValues}
+                                onSubmit={async (values, {resetForm}) => {
                                     await new Promise((r) => setTimeout(r, 500));
-                                    this.sendEmail(JSON.stringify(values));
+                                    this.sendEmail(JSON.stringify(values, {resetForm}));
+
                                 }}
                             >
                                 {({errors, touched, validateField, validateForm}) => (
                                     <Form>
 
-                                        <h4 className="font-weight-light text-uppercase">Twoje dane kontaktowe:</h4>
-                                        <div className="form-row">
+                                        <h4 className="font-weight-light text-uppercase mt-4">Twoje dane
+                                            kontaktowe:</h4>
+                                        <div className="form-row pb-4 mt-4">
                                             <div className="col-12 col-md-3 mb-2">
                                                 <Field id="name"
                                                        name="name"
@@ -83,7 +88,10 @@ class OfferFormik extends Component {
                                                        className="form-control form-control btn-outline-warning"
                                                        placeholder="Imię..."
                                                        validate={(value) => this.validateUsername(value)} required/>
-                                                {errors.name && touched.name && <div>{errors.name}</div>}
+                                                {errors.name && touched.name && <div>
+                                                    <small
+                                                        className='alert-danger border border-danger mt-1 p-1 rounded-bottom'>{errors.name}</small>
+                                                </div>}
                                             </div>
                                             <div className="col-12 col-md-6 mb-2">
                                                 <Field id="email"
@@ -92,39 +100,36 @@ class OfferFormik extends Component {
                                                        className="form-control form-control btn-outline-warning"
                                                        placeholder="Adres email..."
                                                        validate={(value) => this.validateEmail(value)} required/>
-                                                {errors.email && touched.email && <div>{errors.email}</div>}
+                                                {errors.email && touched.email && <div>
+                                                    <small
+                                                        className='alert-danger border border-danger mt-1 p-1 rounded-bottom'>{errors.email}</small>
+                                                </div>}
 
                                             </div>
                                             <div className="col-12 col-md-3 mb-2">
                                                 <Field id="tel" name="tel" type="tel"
                                                        className="form-control form-control btn-outline-warning"
                                                        placeholder="Numer telefonu..."/>
-                                                {errors.tel && touched.tel && <div>{errors.tel}</div>}
                                             </div>
 
                                         </div>
 
-                                        <h4 className="font-weight-light text-uppercase">Czego szukasz:</h4>
-                                        <div className="form-row">
+                                        <h4 className="font-weight-light text-uppercase mt-4">Czego szukasz:</h4>
+                                        <div className="form-row mt-4">
                                             <div className="col-12 col-md-3 mb-2">
                                                 <Field id="budget" name="budget" type="text"
                                                        className="form-control form-control btn-outline-warning"
                                                        placeholder="Budżet..."/>
-                                                {errors.budget && touched.budget && <div>{errors.budget}</div>}
                                             </div>
                                             <div className="col-12 col-md-6 mb-2">
                                                 <Field id="localisation" name="localisation" type="text"
                                                        className="form-control form-control btn-outline-warning"
                                                        placeholder="Lokalizacja..."/>
-                                                {errors.localisation && touched.localisation &&
-                                                <div>{errors.localisation}</div>}
                                             </div>
                                             <div className="col-12 col-md-3 mb-2">
                                                 <Field id="field" name="field" type="text"
                                                        className="form-control form-control btn-outline-warning"
                                                        placeholder="Powierzchnia w m&sup2;..."/>
-                                                {errors.field && touched.field && <div>{errors.field}</div>}
-
                                             </div>
                                         </div>
                                         <div className="input-group mb-3 mt-3">
